2aab3b0afca5af17e650043847e219c01faa9318
SylTana <156739100+SylTana@users.noreply.github.com>
2024-12-04 18:56:32 +0800

    Delete test directory

--- test/test/php/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // Start the session
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-
-// Redirect to home page or login page
-header("Location: " . $_SERVER['HTTP_REFERER']);
-exit();
-?>
